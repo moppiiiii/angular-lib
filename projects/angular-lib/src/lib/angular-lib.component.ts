d777d08612eb9fd70280b0e1494a55047584e088
moppiiiii <tnktmhssrc@gmail.com>
2025-02-18 09:24:57 +0900

    init

---
+++ projects/angular-lib/src/lib/angular-lib.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-angular-lib',
+  standalone: true,
+  imports: [],
+  template: `
+    <p>
+      angular-lib works!
+    </p>
+  `,
+  styles: ``
+})
+export class AngularLibComponent {
+
+}
---
+++ projects/angular-lib/src/public-api.ts
@@ -0,0 +1,6 @@
+/*
+ * Public API Surface of angular-lib
+ */
+
+export * from './lib/angular-lib.service';
+export * from './lib/angular-lib.component';
